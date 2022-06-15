7f331713b7176e309ae120daab2e10f179a9a93e
hasanardhian8 <hasanardhian11@gmail.com>
2022-06-15 13:09:20 +0700

    tambah

---
+++ backend/server/routes/UserRoute.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import authJWT from "../helpers/authJWT";
+import IndexController from "../controller/IndexController"
+
+const router = Router()
+
+router.post("/signin", authJWT.authenticate, authJWT.login);
+router.post("/signup", IndexController.UserController.signup);
+
+export default router;
